4559e6f5f0bb0a59ec62564f7a9541bb9e2fecdf
Denis <denisvovchenko96@mail.ru>
2018-06-19 15:55:02 +0200

    Стилизация и оживление главной страницы
    Подчеркивания не знаю как реализовать

--- js/script.js
+++ js/script.js
@@ -5,25 +5,6 @@ var sliderPagButtons 	= document.querySelectorAll('.slider-pagination button');
 var sliders 					=	document.querySelectorAll('.slider-item');
 var body 							= document.querySelector('body');
 
-
-// var hiddenElement 		= document.querySelector('.catalog-list');
-// // console.log(hiddenElement);
-// // var hoveredElement 		= document.querySelector('.site-nav [href]:hover');
-// // console.log(hoveredElement);
-// // var normElement 			= document.querySelector('.site-nav-catalog a');
-// 	// console.log(hiddenElement.previousElementSibling);
-// 	var anchor = document.querySelector('.site-nav-catalog a');
-// 	// console.log(anchor);
-// 	console.log(hiddenElement.style.display);
-
-// // while (hiddenElement.style.display !== 'none') {
-// // 	// anchor.style.color = '#333';
-// // 	// hiddenElement.previousElementSibling.style.backgroundColor = '#f7f6f3';
-// // }
-
-
-
-
 for (let i = 0; i < sliderPagButtons.length; i++) {
 
 	sliderPagButtons[i].addEventListener('click', function() {
@@ -39,18 +20,16 @@ function toggleSlider(btn) {
 		};
 
 	if (btn == 0) {
-			body.style.background = '#849d8f url("../img/slider-img/slider-1.png") no-repeat center top';
+			body.style.background = '#849d8f url("img/slider-img/slider-1.png") no-repeat center top';
 		} else if (btn == 1) {
-			body.style.background = '#8996a6 url("../img/slider-img/slider-2.png") no-repeat center top';
+			body.style.background = '#8996a6 url("img/slider-img/slider-2.png") no-repeat center top';
 		} else if (btn == 2) {
-			body.style.background = '#9d8b84 url("../img/slider-img/slider-3.png") no-repeat center top';
-		}
+			body.style.background = '#9d8b84 url("img/slider-img/slider-3.png") no-repeat center top';
+		};
 
 	sliderPagButtons[btn].classList.add('slider-pagination-active');
 	sliders[btn].classList.add('slider-item-active');
 };
-
-
 
 feedbackBtn.addEventListener('click', function() {
 	modalWindow.style.display = 'block';
@@ -59,4 +38,3 @@ feedbackBtn.addEventListener('click', function() {
 closeModal.addEventListener('click', function() {
 	modalWindow.style.display = 'none';
 });
-
