e002ddda6785695193308e1bd7a292576ef5e589
Denis <denisvovchenko96@mail.ru>
2018-06-27 19:15:24 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-var sliderPagButtons 	= document.querySelectorAll('.slider-pagination button');
-var sliders 					=	document.querySelectorAll('.slider-item');
-var body 							= document.querySelector('body');
-
 var sliderImage1 			= new Image(1045, 881);
 		sliderImage1.src 	= 'img/slider-img/slider-1.png';
 var sliderImage2			= new Image(1045, 881);
 		sliderImage2.src 	=	'img/slider-img/slider-2.png';
 var sliderImage3			= new Image(1045, 881);
-		sliderImage3.src 	= 'img/slider-img/slider-3.png'
+		sliderImage3.src 	= 'img/slider-img/slider-3.png';
+
+var sliderPagButtons 	= document.querySelectorAll('.slider-pagination button');
+var sliders 					=	document.querySelectorAll('.slider-item');
+var body 							= document.querySelector('body');
+var paginationButtons = document.querySelectorAll('.pagination a');
+var pagPrevBtn 				= document.querySelector('.pag-prev-btn');
+var pagNextBtn 				= document.querySelector('.pag-next-btn');
+
+
 
 for (let i = 0; i < sliderPagButtons.length; i++) {
 
@@ -35,38 +40,94 @@ function toggleSlider(btn) {
 	sliders[btn].classList.add('slider-item-active');
 };
 
-// function showMap() {
-// 	var mapOptions = {
-// 		center: new google.maps.LatLng(59.938795, 30.323085),
-// 		zoom: 100;
-// 	};
 
-// 	var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-// }
+
+
 
 var feedbackBtn 			= document.querySelector('.open-feedback-btn');
-var modalWindow 			= document.querySelector('.feedback-modal');
-var closeModal 				= document.querySelector('.modal-close');
+var popup 						= document.querySelector('.feedback-modal');
+var popupWindow 			= popup.querySelector('.modal-inner');
+var overlay 	 				= document.querySelector('.overlay');
+var closeModal 				= popup.querySelector('.modal-close');
+var form 							= popup.querySelector('.feedback-form');
+var userName 					= document.querySelector('.feedback-name-field');
+var userEmail 				= document.querySelector('.feedback-email-field');
+var userMessage				= document.querySelector('.user-message-area');
 
-if(feedbackBtn) {
-	feedbackBtn.addEventListener('click', function() {
-		modalWindow.style.display = 'flex';
+var isStorageSupport 		= true;
+var nameStorage				= '';
+var emailStorage 			= '';
+
+try {
+	nameStorage				= localStorage.getItem('name');
+	emailStorage 			= localStorage.getItem('email');
+} catch (err) {
+	isStorageSupport 	= false;
+}
+
+
+if (feedbackBtn) {
+
+	feedbackBtn.addEventListener('click', function(evt) {
+		evt.preventDefault();
+		popup.classList.add('modal-show');
+		overlay.classList.add('overlay-show');
+
+		
+		if (nameStorage && emailStorage) {
+			userName.value 	= nameStorage;
+			userEmail.value = emailStorage;
+			userMessage.focus();
+		} else if (nameStorage) {
+			userName.value 	= nameStorage;
+			emailStorage.focus();
+		} else {
+			userName.focus();	
+		}
 	});
 }
 
 if(closeModal) {
-	closeModal.addEventListener('click', function() {
-		modalWindow.style.display = 'none';
+	closeModal.addEventListener('click', function(evt) {
+		evt.preventDefault();
+		popup.classList.remove('modal-show');
+		overlay.classList.remove('overlay-show');
+		popupWindow.classList.remove('modal-error');
+	});
+}
+
+if (form) {
+	form.addEventListener('submit', function(evt) {
+		if (!userName.value || !userEmail.value || !userMessage.value) {
+			evt.preventDefault();
+			popupWindow.classList.remove('modal-error');
+			popupWindow.offsetWidth = popupWindow.offsetWidth;
+			popupWindow.classList.add('modal-error');
+		} else {
+			if (isStorageSupport) {
+				localStorage.setItem('name', userName.value);
+				localStorage.setItem('email', userEmail.value);	
+			}		
+		}
 	});
 }
 
 
+window.addEventListener('keydown', function (evt) {
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
 
-var paginationButtons = document.querySelectorAll('.pagination a');
-var pagPrevBtn = document.querySelector('.pag-prev-btn');
-var pagNextBtn = document.querySelector('.pag-next-btn');
+		if (popup.classList.contain('modal-show')) {
+			popup.classList.remove('modal-show');
+			popupWindow.classList.remove('modal-error');
+			overlay.classList.remove('overlay-show');
+		}
+	}
+});
 
-console.log(paginationButtons);
+
+
+
 
 for (var i = 0; i < paginationButtons.length; i++) {
 
@@ -98,15 +159,3 @@ for (var i = 0; i < paginationButtons.length; i++) {
 		}
 	});
 }
-
-// if (pagPrevBtn) {
-// 	pagPrevBtn.addEventListener('click', function() {
-
-
-
-// 		for (var i = 0; i < paginationButtons.length; i++) {
-// 			paginationButtons[i].classList.remove('pagination-active');
-// 		}
-// 	});
-
-// }
